656f6e06d6b3f3cb3f6d74d7a1f3d25c66089f83
Himanshu Gupta <Himanshu.Gupta14@aexp.com>
2020-09-16 19:23:47 +0530

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use couchbase::*;
 use actix_web::{get, web, App, HttpResponse, HttpServer};
 
+struct PaceCouchbase {
+    collection: Collection,
+}
 #[get("/getDetails/{id}")]
-async fn index(web::Path(id): web::Path<String>, collection: web::Data<Collection>) -> HttpResponse {
-    collection.get(id, GetOptions::default()).await {
-        Ok(mut result) => Ok(HttpResponse::Ok().body(result)
+async fn index(web::Path(id): web::Path<String>, paceCouchbase: web::Data<PaceCouchbase>) {
+    paceCouchbase.collection.get(id, GetOptions::default()).await {
+        Ok(mut result) => Ok(HttpResponse::Ok().body(result.content)
         Err(e) => Ok(HttpResponse::InternalServerError().content_type("text/plain").body(e))),
     };
 }
@@ -13,9 +16,8 @@ async fn index(web::Path(id): web::Path<String>, collection: web::Data<Collectio
 async fn main() -> std::io::Result<()> {
     env_logger::init();
     // Use the default collection (needs to be used for all server 6.5 and earlier)
-    let collection = Cluster::connect("couchbase://127.0.0.1", "Administrator", "password").bucket("travel-sample").default_collection();
    
-    HttpServer::new(|| App::new().app_data(collection).service(index))
+    HttpServer::new(|| App::new().data(PaceCouchbase {collection: Cluster::connect("couchbase://127.0.0.1", "Administrator", "password").bucket("travel-sample").default_collection()}).service(index))
         .bind("127.0.0.1:8080")?
         .run()
         .await
