69a0ead14abcf33d4036dd026076bcd5612cffba
Himanshu Gupta <himanshu.mps@gmail.com>
2021-06-04 19:27:17 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ use couchbase::*;
 use java_properties::read;
 use std::fs::File;
 use std::io::BufReader;
-use std::sync::Arc;
 
+pub struct PaceCouchbase {
+    collection: Collection,
+}
 #[get("/getDetails/{id}")]
 async fn index(
     web::Path(id): web::Path<String>,
-    pace_bucket: web::Data<Arc<Bucket>>,
+    pace_couchbase: web::Data<PaceCouchbase>,
 ) -> Result<HttpResponse, Error> {
-    let results = match pace_bucket
-        .default_collection()
+    let results = match pace_couchbase
+        .collection
         .get(id, GetOptions::default())
         .await
     {
@@ -30,16 +32,20 @@ async fn main() -> std::io::Result<()> {
         let file_name = "couchbase.properties";
         let file = File::open(&file_name).unwrap();
         let couchbase_map = read(BufReader::new(file)).unwrap();
-        let tmp_bucket = Cluster::connect(
+        let couchbase_default_collection = Cluster::connect(
             couchbase_map
                 .get::<str>(&"connection_string".to_string())
                 .unwrap(),
             couchbase_map.get::<str>(&"username".to_string()).unwrap(),
             couchbase_map.get::<str>(&"password".to_string()).unwrap(),
         )
-        .bucket(couchbase_map.get::<str>(&"bucket".to_string()).unwrap());
-        let arc_bucket = Arc::new(tmp_bucket);
-        App::new().data(arc_bucket.clone()).service(index)
+        .bucket(couchbase_map.get::<str>(&"bucket".to_string()).unwrap())
+        .default_collection();
+        App::new()
+            .data(PaceCouchbase {
+                collection: couchbase_default_collection,
+            })
+            .service(index)
     })
     .bind("127.0.0.1:8080")?
     .run()
