d4718f6ce497578432a8909e1a3142ddc2d67334
Himanshu Gupta <himanshu.mps@gmail.com>
2021-06-05 00:10:55 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn main() -> std::io::Result<()> {
     let arc_bucket = ;
     //let cb_collection = arc_bucket.default_collection();
     //let arc_collection = Arc::new(cb_collection);
-    HttpServer::new(move || App::new().data(Arc::new(cb_bucket)).service(index))
+    HttpServer::new(move || App::new().data(Arc::new(cb_bucket).clone()).service(index))
         .bind("0.0.0.0:8082")?
         .run()
         .await
