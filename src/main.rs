079dd8575eff4337319492c2efcc449af7eb809a
Himanshu Gupta <himanshu.mps@gmail.com>
2021-06-05 00:08:19 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 use actix_web::{get, web, App, Error, HttpResponse, HttpServer, Result};
-use couchbase::*;
+use couchbase::{Bucket, Cluster, GetOptions};
 use std::env;
 use std::io::BufReader;
 use std::sync::Arc;
-pub struct PaceCouchbase {
-    collection: Arc<couchbase::Collection>,
-}
+
 #[get("/getDetails/{id}")]
 async fn index(
     web::Path(id): web::Path<String>,
-    pace_couchbase: web::Data<PaceCouchbase>,
+    bucket: web::Data<Arc<Bucket>>,
 ) -> Result<HttpResponse, Error> {
-    let results = match pace_couchbase
-        .get_ref()
-        .to_owned()
-        .clone()
-        .collection
+    let results = match bucket
+        .as_ref()
+        .default_collection()
         .get(id, GetOptions::default())
         .await
     {
@@ -26,6 +22,7 @@ async fn index(
 }
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info,actix_server=info");
     env_logger::init();
     let cb_cluster = Cluster::connect(
         env::var("COUCHBASE_STRING").unwrap(),
@@ -34,17 +31,11 @@ async fn main() -> std::io::Result<()> {
     );
     let arc_cluster = Arc::new(cb_cluster);
     let cb_bucket = arc_cluster.bucket(env::var("COUCHBASE_BUCKET").unwrap());
-    let arc_bucket = Arc::new(cb_bucket);
-    let cb_collection = arc_bucket.default_collection();
-    let arc_collection = Arc::new(cb_collection);
-    HttpServer::new(move || {
-        App::new()
-            .data(PaceCouchbase {
-                collection: arc_collection.clone(),
-            })
-            .service(index)
-    })
-    .bind("0.0.0.0:8082")?
-    .run()
-    .await
+    let arc_bucket = ;
+    //let cb_collection = arc_bucket.default_collection();
+    //let arc_collection = Arc::new(cb_collection);
+    HttpServer::new(move || App::new().data(Arc::new(cb_bucket)).service(index))
+        .bind("0.0.0.0:8082")?
+        .run()
+        .await
 }
