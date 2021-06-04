cec9505f1c44868617e7266de74663f1bded0275
Himanshu Gupta <himanshu.mps@gmail.com>
2021-06-05 00:38:47 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -31,9 +31,6 @@ async fn main() -> std::io::Result<()> {
     );
     let arc_cluster = Arc::new(cb_cluster);
     let cb_bucket = arc_cluster.bucket(env::var("COUCHBASE_BUCKET").unwrap());
-    let arc_bucket = ;
-    //let cb_collection = arc_bucket.default_collection();
-    //let arc_collection = Arc::new(cb_collection);
     HttpServer::new(move || App::new().data(Arc::new(cb_bucket).clone()).service(index))
         .bind("0.0.0.0:8082")?
         .run()
