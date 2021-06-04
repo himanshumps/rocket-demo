7df22fe49d6ffcb57f1c8d144ec40e3041c7656a
Himanshu Gupta <himanshu.mps@gmail.com>
2021-06-05 01:53:33 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use actix_web::{get, web, App, Error, HttpResponse, HttpServer, Result};
+use actix_web::{get, middleware::Logger, web, App, Error, HttpResponse, HttpServer, Result};
 use couchbase::{Bucket, Cluster, GetOptions};
 use std::env;
-use std::io::BufReader;
 use std::sync::Arc;
 
 #[get("/getDetails/{id}")]
@@ -9,7 +8,7 @@ async fn index(
     web::Path(id): web::Path<String>,
     bucket: web::Data<Arc<Bucket>>,
 ) -> Result<HttpResponse, Error> {
-    let results = match bucket
+    let results = bucket
         .as_ref()
         .default_collection()
         .get(id, GetOptions::default())
@@ -22,8 +21,7 @@ async fn index(
 }
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info,actix_server=info");
-    env_logger::init();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
     let cb_cluster = Cluster::connect(
         env::var("COUCHBASE_STRING").unwrap(),
         env::var("COUCHBASE_USERNAME").unwrap(),
@@ -32,8 +30,13 @@ async fn main() -> std::io::Result<()> {
     let arc_cluster = Arc::new(cb_cluster);
     let cb_bucket = arc_cluster.bucket(env::var("COUCHBASE_BUCKET").unwrap());
     let arc_bucket = Arc::new(cb_bucket);
-    HttpServer::new(move || App::new().data(arc_bucket.clone()).service(index))
-        .bind("0.0.0.0:8082")?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .data(arc_bucket.clone())
+            .wrap(middleware::Logger::default())
+            .service(index)
+    })
+    .bind("0.0.0.0:8082")?
+    .run()
+    .await
 }
