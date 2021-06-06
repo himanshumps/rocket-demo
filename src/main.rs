7fe483aeec945849e74ef74cf3cd2db18c6d1d6a
Himanshu Gupta <himanshu.mps@gmail.com>
2021-06-07 01:45:27 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use actix_web::{get, middleware::Logger, web, App, Error, HttpResponse, HttpServer, Result};
-use couchbase::{Bucket, Cluster, GetOptions};
 use std::env;
-use std::sync::Arc;
+
+use actix_http::KeepAlive;
+use actix_web::{App, get, HttpResponse, HttpServer, middleware::Logger, Result, web};
+use couchbase::{Cluster, Collection, GetOptions};
+
+/*use async_std::sync::Arc;*/
+struct PaceCouchbase {
+    pace_collection: Collection,
+}
 
 #[get("/getDetails/{id}")]
 async fn index(
     web::Path(id): web::Path<String>,
-    bucket: web::Data<Arc<Bucket>>,
-) -> Result<HttpResponse, Error> {
-    let results = match bucket
-        .as_ref()
-        .default_collection()
+    pace_couchbase: web::Data<PaceCouchbase>,
+) -> Result<HttpResponse, HttpResponse> {
+    let results = match pace_couchbase
+        .pace_collection
         .get(id, GetOptions::default())
         .await
     {
@@ -19,24 +24,38 @@ async fn index(
     };
     Ok(results)
 }
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
-    let cb_cluster = Cluster::connect(
-        env::var("COUCHBASE_STRING").unwrap(),
-        env::var("COUCHBASE_USERNAME").unwrap(),
-        env::var("COUCHBASE_PASSWORD").unwrap(),
-    );
-    let arc_cluster = Arc::new(cb_cluster);
-    let cb_bucket = arc_cluster.bucket(env::var("COUCHBASE_BUCKET").unwrap());
-    let arc_bucket = Arc::new(cb_bucket);
-    HttpServer::new(move || {
+    /*
+        let cb_cluster = Cluster::connect(
+            env::var("COUCHBASE_STRING").unwrap(),
+            env::var("COUCHBASE_USERNAME").unwrap(),
+            env::var("COUCHBASE_PASSWORD").unwrap(),
+        );
+        let arc_cluster = Arc::new(cb_cluster);
+        let cb_bucket = arc_cluster.bucket(env::var("COUCHBASE_BUCKET").unwrap());
+        let arc_bucket = Arc::new(cb_bucket);
+    */    HttpServer::new(move || {
         App::new()
-            .data(arc_bucket.clone())
+            .data(PaceCouchbase {
+                pace_collection: Cluster::connect(
+                    env::var("COUCHBASE_STRING").unwrap(),
+                    env::var("COUCHBASE_USERNAME").unwrap(),
+                    env::var("COUCHBASE_PASSWORD").unwrap(),
+                )
+                    .bucket(env::var("COUCHBASE_BUCKET").unwrap())
+                    .default_collection()
+            })
             .wrap(Logger::default())
             .service(index)
     })
-    .bind("0.0.0.0:8082")?
-    .run()
-    .await
+        .keep_alive(KeepAlive::Os)
+        .client_timeout(0)
+        .backlog(1024)
+        .workers(num_cpus::get() * 8)
+        .bind("0.0.0.0:8082")?
+        .run()
+        .await
 }
